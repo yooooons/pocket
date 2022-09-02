736b5d303c7a604c78f17d04e6e218646a23f87d
yoon <yooooonnf@gmail.com>
2022-09-02 19:20:21 +0900

    SPRING BOOT 프로젝트 생성

---
+++ src/main/java/com/yoon/pocket/PocketApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.pocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketApplication.class, args);
+	}
+
+}

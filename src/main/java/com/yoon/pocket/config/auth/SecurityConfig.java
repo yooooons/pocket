40b36f5afce4f15620cafb0079489ead082db163
yoon <yooooonnf@gmail.com>
2022-09-06 00:30:32 +0900

    feat : SecurityConfig 추가(#2)

---
+++ src/main/java/com/yoon/pocket/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.yoon.pocket.config.auth;
+
+import com.yoon.pocket.entity.user.Role;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()//h2-condole 화면을 사용하기위해서 옵션 끄기
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers("/", "/h2-console/**").permitAll()
+                .antMatchers("/api/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+
+        return http.build();
+    }
+
+}

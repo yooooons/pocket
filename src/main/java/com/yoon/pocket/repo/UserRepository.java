56cbd367b554cd40374698a84fc2a6e5c1744ee1
yoon <yooooonnf@gmail.com>
2022-09-08 18:41:24 +0900

    feat : user 레파지토리 생성(#1)

---
+++ src/main/java/com/yoon/pocket/repo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.yoon.pocket.repo;
+
+import com.yoon.pocket.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+}

729d0c71487cb3dadfac20b2109d08c0bb1eeeac
yoon <yooooonnf@gmail.com>
2022-09-15 14:39:13 +0900

    fix : changeCostBalance수정 (#3)

--- src/main/java/com/yoon/pocket/entity/pocket/Pocket.java
+++ src/main/java/com/yoon/pocket/entity/pocket/Pocket.java
@@ -1,6 +1,9 @@
 package com.yoon.pocket.entity.pocket;
 
 import com.yoon.pocket.entity.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
@@ -9,12 +12,15 @@ import javax.validation.constraints.Min;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class Pocket {
     @Id
     @GeneratedValue
     private Long id;
 
-    @Column(nullable = true)
+    @Column(nullable = false)
     @Min(value = 0L, message = "0원 이상만 가능합니다.")
     private Long startBudget;
 
@@ -27,6 +33,15 @@ public class Pocket {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    public void changeCostBalance(Long cost) {
+        if (this.balance == null) {
+            this.balance = this.startBudget - this.cost;
+        }
+        this.cost += cost;
+        this.balance -= cost;
+    }
+
 
 
 
